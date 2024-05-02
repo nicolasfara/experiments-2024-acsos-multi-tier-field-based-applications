31d0d284e2b31e84a3b524d6bed42ca82d509015
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-02 12:55:16 +0200

    Add simple hop count

--- src/main/scala/it/unibo/modularization/EmergencyService.scala
+++ src/main/scala/it/unibo/modularization/EmergencyService.scala
@@ -2,7 +2,8 @@ package it.unibo.modularization
 
 class EmergencyService extends MyAggregateProgram {
 
-  override def main(): Any = {
-    rep(0.0)(_ + 1)
+  override def main() = {
+    val hopCount: Int = G[Int](mid() == 80, 0, acc => acc + 1, nbrRange _)
+    writeEnv("hopCount", hopCount)
   }
 }
